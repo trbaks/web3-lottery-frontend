bb98b29d96ef25659cbc8d0e35ef95804a4882e8
trbaks <trbaks@gmail.com>
2022-06-13 16:52:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+const App = () => {
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState("");
+  const [balance, setBalance] = useState("0");
+  const [value, setValue] = useState("0.011");
+  const [status, setStatus] = useState();
+  const [isManager, setIsManager] = useState(false);
 
-function App() {
+  useEffect(() => {
+    getManager();
+    getPlayers();
+    getBalance();
+  }, [balance]);
+
+  const getManager = async () => {
+    const manager = await lottery.methods.manager().call();
+    console.log(`Value of manager is ${manager}`);
+    setManager(manager);
+    const accounts = await web3.eth.getAccounts();
+    if (accounts[0] === manager) {
+      setIsManager(true);
+    }
+  };
+  const getPlayers = async () => {
+    const players = await lottery.methods.getPlayers().call();
+    setPlayers(players);
+  };
+  const getBalance = async () => {
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    setBalance(balance);
+  };
+  const inputHandler = event => {
+    setValue(event.target.value);
+    console.log(value);
+  };
+  const submitHandler = async event => {
+    event.preventDefault();
+    try {
+      setStatus("Waiting on transaction success......");
+      const accounts = await web3.eth.getAccounts();
+      const result = await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, "ether")
+      });
+      setStatus(
+        "Transaction Successful! Your entry has been accepted. Good Luck!"
+      );
+      getBalance();
+    } catch (err) {
+      console.log(err);
+      setStatus(err);
+    }
+  };
+
+  const pickWinner = async () => {
+    try {
+      setStatus("Waiting for the transaction to be mined...");
+      const accounts = await web3.eth.getAccounts();
+      await lottery.methods.pickWinner().send({
+        from: accounts[0]
+      });
+      setStatus("Mining Complete. Transaction successful. Lottery Winner has been picked!");
+      getBalance();
+    } catch (err) {
+      console.log(err);
+      setStatus("Something went wrong.... please try again!");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Web 3.0</h2>
+      <h3>This contract is managed by Manager# {manager} </h3>
+      <br></br>
+      <i>
+        Current there are <b>{players.length}</b> players in lottery and current
+        balance is <b>{web3.utils.fromWei(balance, "ether")} ether</b>
+      </i>
+      <hr></hr>
+      <form onSubmit={submitHandler}>
+        <h4>Want to try your luck?</h4>
+        <label>
+          Enter the amount in ether <i>(Minimum amount 0.011 ether)</i>{" "}
+        </label>
+        <input value={value} onChange={inputHandler}></input>
+        <br />
+        <br />
+        <button>Enter</button>
+      </form>
+      <hr></hr>
+      {status}
+
+      <hr />
+      {isManager && <button onClick={pickWinner}>Pick Winner</button>}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,73 @@
+import web3 from './web3';
+import lottery from './lottery';
+
+//Address of contract deployed on Etherium network
+const address = process.env.REACT_APP_CONTRACT_ADDRESS;
+const abi = [
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"internalType": "address payable[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+export default new web3.eth.Contract(abi, address);
